aa4ec50eff7397739222260e9b587b1f341b9e46
Ansh Kakkar <Ansh.kakkar@warriorlife.net>
2017-12-13 11:08:02 -0800

    more functions and a catch
    added function
    added catch and throw so no invalid input

--- calculator.js
+++ calculator.js
@@ -10,3 +10,12 @@ function v(val) {
 	document.getElementById("d").value += val;
 }
 
+// Evaluate the equation 
+function e() {
+	try{
+		d(eval(document.getElementById("d").value));
+	}
+	catch(err) {
+		d("Error");
+	}
+}
